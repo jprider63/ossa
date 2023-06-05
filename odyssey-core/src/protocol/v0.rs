9bdcced667ed01eb7d129c8503a8b54d44b303a6
James Parker <dev@jamesparker.me>
2023-06-05 07:32:03 -0400

    Initial protocol for retrieving store metadata

--- odyssey-core/src/protocol/v0.rs
+++ odyssey-core/src/protocol/v0.rs
@@ -1,14 +1,33 @@
 
-// Network level protocols between peers
+use async_session_types::{Eps, Send, Recv};
+
+use crate::store;
+
+// # Protocols run between peers.
+
+// request_store_metadata_header :: RequestStoreMetadataHeaderV0 -> Either<ProtocolError, ResponseStoreMetadataHeaderV0>
+pub type StoreMetadataHeader<TypeId, StoreId> = Send<StoreMetadataHeaderRequest<StoreId>,Recv<ProtocolResult<StoreMetadataHeaderResponse<TypeId, StoreId>>, Eps>>;
+pub type StoreMetadataBody = Send<StoreMetadataBodyRequest,Recv<ProtocolResult<StoreMetadataBodyResponse>, Eps>>;
+
 //
 // TODO: Do we need to do a handshake to establish a MAC key (is an encryption needed)?.
 //
-// pub struct RequestStoreMetadataHeaderV0 {
-//   store_id: store::Id,
-//   body_request: RequestStoreMetadataBodyV0,
-// }
-//
-// pub struct RequestStoreMetadataBodyV0 {
-// }
-//
-// request_store_metadata_header :: RequestStoreMetadataHeaderV0 -> Either<ProtocolError, ResponseStoreMetadataHeaderV0>
+
+// # Messages sent by protocols.
+
+pub struct StoreMetadataHeaderRequest<StoreId> {
+    store_id: StoreId,
+    body_request: Option<StoreMetadataBodyRequest>,
+}
+
+pub struct StoreMetadataHeaderResponse<TypeId, StoreId> {
+    header: store::v0::MetadataHeader<TypeId, StoreId>,
+    body: Option<StoreMetadataBodyResponse>,
+}
+
+pub type StoreMetadataBodyRequest = (); // TODO: Eventually request certain chunks.
+pub type StoreMetadataBodyResponse = store::v0::MetadataBody; // TODO: Eventually request certain chunks.
+
+pub type ProtocolResult<T> = Result<T, ProtocolError>;
+pub type ProtocolError = String; // TODO: Eventually more informative error type.
+
